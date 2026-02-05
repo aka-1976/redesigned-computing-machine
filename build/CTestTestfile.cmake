3b91918531199674c556d32f306f74467d2a52d9
Mustapha Barki <aka1976mb@outlook.com>
2026-02-05 01:20:15 +0000

    Add Visual Studio project files for redesigned computing machine
    - Created main.vcxproj.filters to organize source and header files.
    - Added redesigned-computing-machine.sln to manage project configurations and dependencies.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/aka1976mb/Documents/GitHub/redesigned-computing-machine
+# Build directory: C:/Users/aka1976mb/Documents/GitHub/redesigned-computing-machine/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
